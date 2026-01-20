22acd5f0e0ec1729c24864da0b1aafe5bb046c6e
Tulkin Bobomirzayev <tulqinjon2001@gmail.com>
2026-01-20 15:05:22 +0500

    _

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "http://localhost:3000/api" ||
   "https://zakaz-backend-production.up.railway.app/api";
 
 const api = axios.create({
